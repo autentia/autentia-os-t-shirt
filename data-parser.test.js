24a2a88b2f8e167aefa222a0f56fd6fdd7efdcf1
César Alberca <cesalberca@gmail.com>
2018-01-23 14:35:07 +0100

    Remove tests which have to do with private API

--- data-parser.test.js
+++ data-parser.test.js
@@ -1,107 +1,5 @@
 const dataParser = require('./data-parser')
 
-describe('findProjectsWithCommonTechs', () => {
-  test('finds projects which have related tech', () => {
-    const projects = [
-      { 
-        name: 'foo',
-        techs: [
-          'bar',
-          'baz',
-        ]
-      },
-      { 
-        name: 'qux',
-        techs: [
-          'bar',
-          'quux',
-        ]
-      }
-    ]
-  
-    const techs = dataParser.findProjectsWithCommonTechs('bar', projects)
-  
-    expect(techs).toEqual(projects)
-  })
-  
-  test('handle no related tech found projects', () => {
-    const projects = [
-      { 
-        name: 'foo',
-        techs: [
-          'bar',
-          'baz',
-        ]
-      },
-      { 
-        name: 'qux',
-        techs: [
-          'quux',
-          'quuux',
-        ]
-      }
-    ]
-  
-    const techs = dataParser.findProjectsWithCommonTechs('x', projects)
-  
-    expect(techs).toEqual([])
-  })
-})
-
-describe('extractRelatedTech', () => {
-  test('parses projects with common tech into an another shape', () => {
-    const projects = [
-      { 
-        name: 'foo',
-        techs: [
-          'bar',
-          'baz',
-        ]
-      },
-      { 
-        name: 'qux',
-        techs: [
-          'bar',
-          'quux',
-        ]
-      }
-    ]
-  
-    const techs = dataParser.extractRelatedTech('bar', projects)
-  
-    expect(techs).toEqual([
-      {
-        name: 'bar',
-        relatedTechs: [
-          'baz',
-          'quux'
-        ]
-      }
-    ])
-  })
-
-  test('parses projects into another shape even if they don\'t have related tech', () => {
-    const projects = [
-      { 
-        name: 'foo',
-        techs: [
-          'bar',
-          'baz',
-        ]
-      }
-    ]
-  
-    const techs = dataParser.extractRelatedTech('bar', projects)
-  
-    expect(techs).toEqual([
-      {
-        name: 'bar',
-        relatedTechs: ['baz']
-      }
-    ])
-  })
-})
-
 describe('extractRelatedTechs', () => {
   test('extract related techs', () => {
     const projects = [
@@ -162,6 +60,43 @@ describe('extractRelatedTechs', () => {
       {
         name: 'baz',
         relatedTechs: ['bar']
+      }
+    ])
+  })
+
+  test('finds complex related techs', () => {
+    const projects = [
+      {
+        name: 'foo',
+        techs: [
+          'bar',
+          'baz',
+        ]
+      },
+      {
+        name: 'qux',
+        techs: [
+          'baz',
+          'bar',
+          'quux'
+        ]
+      }
+    ]
+  
+    const techs = dataParser.extractRelatedTechs(projects)
+  
+    expect(techs).toEqual([
+      {
+        name: 'bar',
+        relatedTechs: ['baz', 'quux']
+      },
+      {
+        name: 'baz',
+        relatedTechs: ['bar', 'quux']
+      },
+      {
+        name: 'quux',
+        relatedTechs: ['baz', 'bar']
       }
     ])
   })
