0bcea0748097b529f06dcd68e38805d34cb85b9c
César Alberca <cesalberca@gmail.com>
2018-01-23 14:35:22 +0100

    Create main

---
+++ main.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const parser = require('./data-parser')
+const formatter = require('./data-formatter')
+
+const csvPath = './data.csv'
+const jsonPath = './data.json'
+
+console.log('Starting...')
+formatter.formatData(csvPath)
+  .then(data => {
+    const techs = parser.extractRelatedTechs(data)
+    fs.writeFileSync(jsonPath, JSON.stringify(data, null, 2))
+    console.log('Done')
+  })
