ff3e6d9173a2b3e19eb53ac833b9ac7e2ceea056
César Alberca <cesalberca@gmail.com>
2018-01-25 16:21:03 +0100

    Some cleanup

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const cluster = d3.cluster().size([360, innerRadius])
 
 const line = d3.radialLine()
   .curve(d3.curveBundle.beta(beta))
-  .radius(function(d) { return d.y })
-  .angle(function(d) { return d.x / 180 * Math.PI })
+  .radius(d => d.y)
+  .angle(d => d.x / 180 * Math.PI)
 
 const svg = d3.select('body').append('svg')
   .attr('width', diameter)
@@ -17,34 +17,32 @@ const svg = d3.select('body').append('svg')
   .append('g')
   .attr('transform', 'translate(' + radius + ',' + radius + ')')
 
-let link = svg.append('g').selectAll('.link'),
-  node = svg.append('g').selectAll('.node')
-
-d3.json('data.json', function(error, classes) {
-  if (error) throw error
-
-  const root = relatedTechsHierarchy(classes)
-    .sum(function(d) { return d.size })
-
-  cluster(root)
   
-  link = link
+  d3.json('data.json', (error, classes) => {
+    const root = relatedTechsHierarchy(classes)
+    .sum(d => d.size)
+    
+    cluster(root)
+
+    let link = svg.append('g').selectAll('.link')
+    link
     .data(getRelatedTechs(root.leaves()))
     .enter().append('path')
-    .each(function(d) { d.source = d[0], d.target = d[d.length - 1] })
+    .each(d => { d.source = d[0], d.target = d[d.length - 1] })
     .attr('class', 'link')
     .attr('d', line)
-
-  node = node
-    .data(root.leaves())
-    .enter().append('text')
-      .attr('class', 'node')
-      .attr('dy', '0.31em')
-      .attr('transform', function(d) { return `rotate(${d.x - 90}) translate(${d.y + 8},0) ${d.x < 180 ? '' : 'rotate(180)'}`})
-      .attr('text-anchor', function(d) { return d.x < 180 ? 'start' : 'end' })
-      .text(function(d) { return d.data.key })
-
-  d3.selectAll('.link')
+    
+    let node = svg.append('g').selectAll('.node')
+    node
+      .data(root.leaves())
+      .enter().append('text')
+        .attr('class', 'node')
+        .attr('dy', '0.31em')
+        .attr('transform', d => `rotate(${d.x - 90}) translate(${d.y + 8},0) ${d.x < 180 ? '' : 'rotate(180)'}`)
+        .attr('text-anchor', d => d.x < 180 ? 'start' : 'end')
+        .text(d => d.data.key)
+      
+    d3.selectAll('.link')
     .each(function(d, i) {
       d3.select(this)
       .attr('style', `stroke: ${palette[Math.trunc(Math.random() * palette.length)]};`)
@@ -58,17 +56,18 @@ function relatedTechsHierarchy(classes) {
     let node = map[name]
     let i
     if (!node) {
-      node = map[name] = data || {name: name, children: []}
+      node = map[name] = data || { name: name, children: [] }
       if (name.length) {
         node.parent = find(name.substring(0, i = name.lastIndexOf('.')))
         node.parent.children.push(node)
+        // Add the tech's name
         node.key = name.substring(i + 1)
       }
     }
     return node
   }
 
-  classes.forEach(function(d) {
+  classes.forEach(d => {
     find(d.name, d)
   })
 
@@ -79,14 +78,16 @@ function getRelatedTechs(nodes) {
   const map = {},
     relatedTechs = []
 
-  nodes.forEach(function(d) {
+  nodes.forEach(d => {
     map[d.data.name] = d
   })
 
-  nodes.forEach(function(d) {
-    if (d.data.relatedTechs) d.data.relatedTechs.forEach(function(i) {
-      relatedTechs.push(map[d.data.name].path(map[i]))
-    })
+  nodes.forEach(d => {
+    if (d.data.relatedTechs) {
+      d.data.relatedTechs.forEach(i => {
+        relatedTechs.push(map[d.data.name].path(map[i]))
+      })
+    }
   })
 
   return relatedTechs
